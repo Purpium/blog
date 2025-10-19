0bf7199e1e1b538f33d7179ad15176178672684f
Prple <63184685+Purpium@users.noreply.github.com>
2025-10-20 01:10:30 +0800

    Update consts

--- src/const.ts
+++ src/const.ts
@@ -17,16 +17,16 @@ export type Socials = {
 }[];
 
 export const SITE: Site = {
-  TITLE: "Astro Micro",
-  DESCRIPTION: "Astro Micro is an accessible and lightweight blog.",
-  EMAIL: "trevortylerlee@gmail.com",
-  NUM_POSTS_ON_HOMEPAGE: 5,
-  NUM_PROJECTS_ON_HOMEPAGE: 3,
+  TITLE: "Prple",
+  DESCRIPTION: "Call me Prple, Purp or Purpium. I go by many names.",
+  EMAIL: "hello@prple.dev",
+  NUM_POSTS_ON_HOMEPAGE: 0,
+  NUM_PROJECTS_ON_HOMEPAGE: 0,
 };
 
 export const HOME: Metadata = {
-  TITLE: "Home",
-  DESCRIPTION: "Astro Micro is an accessible theme for Astro.",
+  TITLE: "Prple's blog",
+  DESCRIPTION: "Welcome to my blog where I share my thoughts and projects.",
 };
 
 export const BLOG: Metadata = {
@@ -34,23 +34,39 @@ export const BLOG: Metadata = {
   DESCRIPTION: "A collection of articles on topics I am passionate about.",
 };
 
+// Idk if I should still make this page here or on the main site.
 export const PROJECTS: Metadata = {
   TITLE: "Projects",
-  DESCRIPTION:
-    "A collection of my projects with links to repositories and live demos.",
+  DESCRIPTION: "A collection of my projects and works.",
 };
 
 export const SOCIALS: Socials = [
   {
     NAME: "X (formerly Twitter)",
-    HREF: "https://twitter.com/boogerbuttcheek",
+    HREF: "https://twitter.com/Purpium",
   },
   {
     NAME: "GitHub",
-    HREF: "https://github.com/trevortylerlee",
+    HREF: "https://github.com/Purpium",
+  },
+  {
+    NAME: "Codeberg",
+    HREF: "https://codeberg.org/Prple",
+  },
+  {
+    NAME: "Twitch",
+    HREF: "https://twitch.tv/prple",
   },
   {
     NAME: "Website",
-    HREF: "https://trevortylerlee.com",
+    HREF: "https://prple.dev",
+  },
+  {
+    NAME: "Youtube - Gaming",
+    HREF: "https://youtube.com/c/prplejam",
+  },
+  {
+    NAME: "Youtube - Tech",
+    HREF: "https://youtube.com/c/prpledev",
   },
 ];
